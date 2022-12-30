e726ee44e217dfb204f979199e5a31a387bd8012
San68bot <san68bot@gmail.com>
2022-12-29 22:02:25 -0500

    Create Sequential.kt

---
+++ AlphaGo/src/main/java/com/san68bot/alphaLib/control/logicControllers/Sequential.kt
@@ -0,0 +1,20 @@
+package com.san68bot.alphaLib.control.logicControllers
+
+class Sequential(private vararg val commands: () -> Unit) {
+    private var counter = 1
+    private var iterator: () -> Boolean = { false }
+    private val size = commands.size
+
+    fun setIterator(iterator: () -> Boolean): Sequential {
+        this.iterator = iterator
+        return this
+    }
+
+    fun update() {
+        commands[counter-1].invoke()
+        if (iterator.invoke()) {
+            counter++
+            if (counter > size) counter = 1
+        }
+    }
+}
